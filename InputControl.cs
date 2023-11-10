a33530112cf6b7f8bd82b51f6b00da6f4ea903dc
michaumar <michal.maruszczak@o2.pl>
2023-11-10 15:26:21 +0100

    clearing

---
+++ InputControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15;
+
+public class InputControl
+{
+    public static double GetNumericValueFromConsole()
+    {
+        while (true)
+        {
+            try
+            {
+                return Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Co żeś tu wpisoł Ciuluu? To jest kalkulator\n" +
+              "Podaj liczbe");
+            }
+        }
+    }
+
+
+    public static string GetOperationFromConsole()
+    {
+        //to jest mega na siłę i to tego gówniane, try przecież też sprawdza czy operacja jest na liście
+        var a = Console.ReadLine();
+        if (a == "+" || a == "-" || a == "*" || a == "/")
+        {
+            Console.WriteLine("Dobre działanie Mordo");
+        }
+        else
+        {
+            Console.WriteLine("Co żeś tu wpisoł Ciuluu? Zamykam.");
+            Environment.Exit(1);
+        }
+        return a;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿// Seeusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using ConsoleApp15;
+
+namespace Lesson;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double result;
+        Console.WriteLine("kalkulator Chopie");
+
+
+        Console.WriteLine("Type figure a");
+        var a = InputControl.GetNumericValueFromConsole();
+
+        Console.WriteLine("Type figure b");
+        var b = InputControl.GetNumericValueFromConsole();
+
+        Console.WriteLine("Choose operation +, -, *, /");
+        var operation = InputControl.GetOperationFromConsole();
+
+        try
+        {
+            switch (operation)
+            {
+                case "+":
+                    result = Calculator.Add(a, b);
+                    Console.WriteLine($"adding selected {a} + {b} = {result}");
+                    break;
+                case "-":
+                    result = Calculator.Subtract(a, b);
+                    Console.WriteLine($"subtraction selected {a} - {b} = {result}");
+                    break;
+                case "*":
+                    result = Calculator.Multiply(a, b);
+                    Console.WriteLine($"multiplication selected {a} * {b} = {result}");
+                    break;
+                case "/":
+                    result = Calculator.Divide(a, b);
+                    Console.WriteLine($"dividing selected {a} / {b} = {result}");
+                    break;
+                default:
+                    Console.WriteLine("Wracej na gruba!!! Złe działanie");
+                    break;
+            }
+
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+
+}
+
+
+
+
+
+
